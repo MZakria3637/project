d584832080af80af988ef0f7cc604396aac737b9
MZakria3637 <zakriamuhammad3637@gmail.com>
2021-10-08 22:02:11 +0500

    2nd commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,41 @@
+import { ContactBookComponent } from './contact-book/contact-book.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { ClientsComponent } from './clients/clients.component';
+import { NewCaseComponent } from './new-case/new-case.component';
+import { ReportsComponent } from './reports/reports.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'main',
     component: MainContentComponent
+  },
+  {
+    path: '',
+    redirectTo: "/main",
+    pathMatch:'full'
+  }
+  , {
+    path: 'reports',
+    component:ReportsComponent
+  },
+  {
+    path: 'new-case',
+    component:NewCaseComponent
+  }
+  , {
+    path: 'client',
+    component:ClientsComponent
+  },
+  {
+    path: "account",
+    component:AccountsComponent
+  },
+  {
+    path: "contact-book",
+    component: ContactBookComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { ReportsComponent } from './reports/reports.component';
+import { NewCaseComponent } from './new-case/new-case.component';
+import { ClientsComponent } from './clients/clients.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { ContactBookComponent } from './contact-book/contact-book.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +20,12 @@ import { MainContentComponent } from './main-content/main-content.component';
     HeaderComponent,
     NavbarComponent,
     FooterComponent,
-    MainContentComponent
+    MainContentComponent,
+    ReportsComponent,
+    NewCaseComponent,
+    ClientsComponent,
+    AccountsComponent,
+    ContactBookComponent
   ],
   imports: [
     BrowserModule,
